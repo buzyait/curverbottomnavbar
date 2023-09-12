8ffa571ecaee6e7855e3fd3e45f6e47480e05f12
Zeynefly <zeyne.muktarieva.ch@gmail.com>
2023-09-12 16:27:48 +0300

    commmin

---
+++ android/app/src/main/kotlin/com/example/curverbottomnavbar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.curverbottomnavbar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/myhomepage.dart
@@ -0,0 +1,28 @@
+// import 'package:flutter/material.dart';
+
+// class Myhomepage extends StatefulWidget {
+//   const Myhomepage({Key? key}) : super(key: key);
+
+//   @override
+//   _MyhomepageState createState() => _MyhomepageState();
+// }
+
+// class _MyhomepageState extends State<Myhomepage> {
+//   bool lights = false;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Center(
+//       child: SwitchListTile(
+//         title: const Text('Lights'),
+//         value: lights,
+//         onChanged: (bool value) {
+//           setState(() {
+//             lights = value;
+//           });
+//         },
+//         secondary: const Icon(Icons.lightbulb_outline),
+//       ),
+//     );
+//   }
+// }
